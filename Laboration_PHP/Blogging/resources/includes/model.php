5343ce97ab549ec232404892b99e22f015d57c90
Victor Bohmies <victor.bohmies@gmail.com>
2019-02-07 14:56:52 +0100

    Removed Blogging

--- Laboration_PHP/Blogging/resources/includes/model.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//Simple array for topics - Övning 4
-
-//2D Associative array for full posts - Övning 9
-
-?>
--- Laboration_PHP/Blogging/resources/views/header.php
+++
@@ -1,4 +0,0 @@
-<body>
-    <header>
-        <h1><?php echo $header; ?></h1>
-    </header>
